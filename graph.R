b294189ad146fa93424feee34654acdf377ebcc9
Bhavya Singh <bhavya.singh2015@vit.ac.in>
2018-05-29 00:24:37 +0530

    data analytics

---
+++ graph.R
@@ -0,0 +1,5 @@
+
+
+avgmarks = c(78,90,60)
+pie(avgmarks)
+barplot(avgmarks, horiz = T)
---
+++ swirl.R
@@ -0,0 +1,5 @@
+#misc commands
+library(swirl) #for online help
+swirl()
+data() #to see in built data sets
+bye()#to lexit swirl
